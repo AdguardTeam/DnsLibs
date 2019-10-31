4d296a6439ec5272a02f72cd9ca39ed33f92ede4
Sergei Gunchenko <s.gunchenko@adguard.com>
2019-10-31 11:55:39 +0300

    common: introduce a possibility to use user-provided logger
    The one can set one of the standard spdlog sink factory method or
    implement their's own one. See ag::set_logger_factory_callback function.

--- common/include/ag_logger.h
+++ common/include/ag_logger.h
@@ -1,27 +1,44 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/sink.h>
 
 namespace ag {
 
-using logger = std::shared_ptr<spdlog::logger>;
+    using logger = std::shared_ptr<spdlog::logger>;
+    using create_logger_cb = std::function<logger(const std::string &logger_name)>;
 
-enum log_level {
-    TRACE = SPDLOG_LEVEL_TRACE,
-    DEBUG = SPDLOG_LEVEL_DEBUG,
-    INFO = SPDLOG_LEVEL_INFO,
-    WARN = SPDLOG_LEVEL_WARN,
-    ERR = SPDLOG_LEVEL_ERROR,
-};
+    enum log_level {
+        TRACE = SPDLOG_LEVEL_TRACE,
+        DEBUG = SPDLOG_LEVEL_DEBUG,
+        INFO = SPDLOG_LEVEL_INFO,
+        WARN = SPDLOG_LEVEL_WARN,
+        ERR = SPDLOG_LEVEL_ERROR,
+    };
 
+    /**
+     * @brief Create a logger with the given name
+     * @param name logger name
+     * @return logger instance
+     */
     logger create_logger(const std::string &name);
+
+    /**
+     * @brief Set a program-wide logging level
+     * @param lvl desired logging level
+     */
     void set_default_log_level(log_level lvl);
+
+    /**
+     * @brief Set a function which produces the loggers
+     * @param cb factory function
+     */
+    void set_logger_factory_callback(create_logger_cb cb);
 
 } // namespace ag
 
 #define errlog(l_, ...) do { (l_)->error(__VA_ARGS__); } while (0)
 #define infolog(l_, ...) do { (l_)->info(__VA_ARGS__); } while (0)
 #define warnlog(l_, ...) do { (l_)->warn(__VA_ARGS__); } while(0)
-#define dbglog(l_, ...) do { if ((l_)->level() <= spdlog::level::debug) (l_)->debug(__VA_ARGS__); } while (0)
-#define tracelog(l_, ...) do { if ((l_)->level() <= spdlog::level::trace) (l_)->trace(__VA_ARGS__); } while (0)
+#define dbglog(l_, ...) do { if ((l_)->should_log(spdlog::level::debug)) (l_)->debug(__VA_ARGS__); } while (0)
+#define tracelog(l_, ...) do { if ((l_)->should_log(spdlog::level::trace)) (l_)->trace(__VA_ARGS__); } while (0)
--- common/src/logger.cpp
+++ common/src/logger.cpp
@@ -1,11 +1,14 @@
 #include <ag_logger.h>
+#include <spdlog/sinks/stdout_sinks.h>
 
 static ag::log_level default_log_level = ag::INFO;
+static ag::create_logger_cb create_logger_callback =
+    [] (const std::string &name) { return spdlog::stdout_logger_mt(name); };
 
 ag::logger ag::create_logger(const std::string &name) {
     ag::logger logger = spdlog::get(name);
     if (logger == nullptr) {
-        logger = spdlog::stdout_logger_mt(name);
+        logger = create_logger_callback(name);
     }
     logger->set_level((spdlog::level::level_enum)default_log_level);
     return logger;
@@ -13,4 +16,8 @@ ag::logger ag::create_logger(const std::string &name) {
 
 void ag::set_default_log_level(ag::log_level lvl) {
     default_log_level = lvl;
+}
+
+void ag::set_logger_factory_callback(create_logger_cb cb) {
+    create_logger_callback = cb;
 }
