ef645a7b2faa64dad1a3063f20989f2c3626cd28
Nikita Gorskikh <n.gorskikh@adguard.com>
2020-08-08 13:52:11 +0300

    Pull request #151: Round cache entry TTL up
    Merge in ADGUARD-CORE-LIBS/dns-libs from fix/clock_issues to master
    
    Squashed commit of the following:
    
    commit 8d2f6dc85d37293c0b58c0e3323237be831ca4fb
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Fri Aug 7 12:22:36 2020 +0300
    
        Revert back to system_clock because we need to report actual system time when an event occured
    
    commit 6ac637bfa8c5bb19195db3dd7293c5c6ffe73932
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Thu Aug 6 19:30:56 2020 +0300
    
        Fix test
    
    commit 9df6336bcf96017129128818e9ea65bb0efc06f3
    Merge: b901623 3edb1ed
    Author: Atlassian Bamboo <bamboo@example.com>
    Date:   Thu Aug 6 19:26:54 2020 +0300
    
        [bamboo] Automated branch merge (from master:3edb1ed77755853bb49f9c07e6486e0da69cb62b)
    
    commit b9016239ae719faf45b29fbb7b90ddcd15dd76d3
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Thu Aug 6 19:13:53 2020 +0300
    
        Use steady_clock for measuring intervals, always round TTLs up to avoid spurious test failures

--- proxy/test/dnsproxy_test.cpp
+++ proxy/test/dnsproxy_test.cpp
@@ -202,7 +202,7 @@ TEST_F(dnsproxy_cache_test, cached_response_ttl_decreases) {
     ASSERT_NO_FATAL_FAILURE(perform_request(proxy, pkt, res));
     ASSERT_TRUE(last_event.cache_hit);
     const uint32_t cached_ttl = ldns_rr_ttl(ldns_rr_list_rr(ldns_pkt_answer(res.get()), 0));
-    ASSERT_LT(cached_ttl, ttl / 2);
+    ASSERT_LE(cached_ttl, ttl / 2);
 }
 
 TEST_F(dnsproxy_cache_test, cached_response_expires) {
