ad45f9a717eb3b0098556c7f91e11b70f5815f33
Bamboo <Bamboo>
2020-08-12 20:54:26 +0300

    Automatic version increment by Bamboo

--- platform/windows/cs/Adguard.Dns/SolutionInfo.cs
+++ platform/windows/cs/Adguard.Dns/SolutionInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Adguard Software Ltd 2009-2020")]
 [assembly: AssemblyTrademark("Copyright © Adguard Software Ltd 2020")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.3.19")]
-[assembly: AssemblyFileVersion("1.3.19")]
+[assembly: AssemblyVersion("1.3.20")]
+[assembly: AssemblyFileVersion("1.3.20")]
