a658581502855b9b9c55dba51b200cd5c1dcff67
Timur Belkin <t.belkin@adguard.com>
2021-01-29 10:41:00 +0300

    Pull request #196: feature/AG-6342(Change 2020 to 2021 everywhere)
    Merge in ADGUARD-CORE-LIBS/dns-libs from feature/AG-6342 to master
    
    Squashed commit of the following:
    
    commit b268050dd6afc8222a0c4d879304e80d49aefe92
    Author: Timur <t.belkin@adguard.com>
    Date:   Thu Jan 28 18:23:02 2021 +0300
    
        feature/AG-6342 removed new line
    
    commit 7d5101d420560abe726146a32688cd50a8575918
    Author: Timur <t.belkin@adguard.com>
    Date:   Thu Jan 28 18:22:15 2021 +0300
    
        feature/AG-6342 revert changes in TestDnsServer
    
    commit 53ee2938aec3d9f81591c9fb7c3b0f90c296de7a
    Author: Timur <t.belkin@adguard.com>
    Date:   Thu Jan 28 18:17:31 2021 +0300
    
        feature/AG-6342 changed 2020 to 2021 year in info files
    
    commit fd7cf9249e98bae1445f78f907e5062c99c84fb6
    Merge: 0f14d5e 044bc65
    Author: Timur <t.belkin@adguard.com>
    Date:   Thu Jan 28 18:11:01 2021 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/adguard-core-libs/dns-libs
    
    commit 0f14d5e3d8672f03466c59237631a829339cd43e
    Merge: 56e3886 eea3656
    Author: Timur <t.belkin@adguard.com>
    Date:   Mon Jan 11 16:40:33 2021 +0300
    
        Merge branch 'master' of ssh://bit.adguard.com:7999/adguard-core-libs/dns-libs
    
    commit 56e38863bd1585621fbd04790906721714f3be36
    Author: t.belkin@adguard.com <t.belkin@adguard.com>
    Date:   Thu Aug 13 11:57:05 2020 +0300
    
        first unit test

--- platform/windows/cs/Adguard.Dns/Adguard.Dns.TestApp/Properties/AssemblyInfo.cs
+++ platform/windows/cs/Adguard.Dns/Adguard.Dns.TestApp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Adguard.Dns.TestApp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
