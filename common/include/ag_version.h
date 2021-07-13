9fb5071ed307db449a3bdf16a1023a1c0515ba53
Bamboo <Bamboo>
2021-07-13 13:26:16 +0300

    skipci: Automatic version increment by Bamboo

--- common/include/ag_version.h
+++ common/include/ag_version.h
@@ -1,2 +1,2 @@
 #pragma once
-#define AG_DNSLIBS_VERSION "1.6.12"
+#define AG_DNSLIBS_VERSION "1.6.13"
--- platform/windows/cs/Adguard.Dns/SolutionInfo.cs
+++ platform/windows/cs/Adguard.Dns/SolutionInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Adguard Software Ltd 2009-2021")]
 [assembly: AssemblyTrademark("Copyright © Adguard Software Ltd 2021")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.6.12")]
-[assembly: AssemblyFileVersion("1.6.12")]
+[assembly: AssemblyVersion("1.6.13")]
+[assembly: AssemblyFileVersion("1.6.13")]
