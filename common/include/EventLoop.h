382147acc729892c4975fa9a172361f9e1dc5abd
Sergey Fionov <sfionov@adguard.com>
2021-12-21 11:05:56 +0300

    Update NativeLibsCommon

---
+++ common/include/EventLoop.h
@@ -0,0 +1,16 @@
+//
+// Created by Sergey Fionov on 21.12.2021.
+//
+
+#ifndef DNSPROXY_FRAMEWORK_EVENTLOOP_H
+#define DNSPROXY_FRAMEWORK_EVENTLOOP_H
+
+
+
+class EventLoop {
+
+};
+
+
+
+#endif //DNSPROXY_FRAMEWORK_EVENTLOOP_H
---
+++ common/src/EventLoop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sergey Fionov on 21.12.2021.
+//
+
+#include "EventLoop.h"
