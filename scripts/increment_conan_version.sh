84d3dc526067eebb01d9d4030a833e5c466f7d6e
Maxim Grupper <m.grupper@adguard.com>
2022-11-09 12:56:06 +0300

    Pull request #463: fix double increment in conan version
    Merge in ADGUARD-CORE-LIBS/dns-libs from fix/double_increment to master
    
    Squashed commit of the following:
    
    commit 21681dc164517ae39b17824e58e1c8abb23710d7
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:53:59 2022 +0200
    
        fix repo name
    
    commit c94adee7c4e374ccfbe4471f5bcff61d58af9c61
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:52:46 2022 +0200
    
        fix repo name
    
    commit 7677f20f6adb46be40147bea135f59b49dcbc2d4
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:51:11 2022 +0200
    
        fix job name
    
    commit c0b9022f36f171e706620ed0e789b23ef65dc73c
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:47:58 2022 +0200
    
        try to fix
    
    commit ec19662de6463958e20aec645720cf545df9f6fa
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:42:40 2022 +0200
    
        add task to bamboo.yaml
    
    commit bf3d0557652ee73d5819a15edd73979429643925
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Nov 9 11:35:21 2022 +0200
    
        fix double increment

--- scripts/increment_conan_version.sh
+++ scripts/increment_conan_version.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-increment_version() {
-  major=${1%%.*}
-  minor=$(echo ${1#*.} | sed -e "s/\.[0-9]*//")
-  revision=${1##*.}
-  echo ${major}.${minor}.$((revision+1))
-}
-
 argument_version=$1
 if [ -z "$argument_version" ]
 then
-  VERSION=$(cat ../platform/android/dnsproxy/lib/build.gradle | grep "version =" | sed -e "s/,.*//g" | sed -e "s/.*://g" | sed -e "s/[' ]//g")
-  echo "Last version was ${VERSION}"
-  NEW_VERSION=$(increment_version ${VERSION})
+  NEW_VERSION=$(cat ../platform/android/dnsproxy/lib/build.gradle | grep "version =" | sed -e "s/,.*//g" | sed -e "s/.*://g" | sed -e "s/[' ]//g")
   echo "New version is ${NEW_VERSION}"
 else
   if [[ "$argument_version" =~ ^[0-9]\.[0-9].[0-9]*$ ]]
