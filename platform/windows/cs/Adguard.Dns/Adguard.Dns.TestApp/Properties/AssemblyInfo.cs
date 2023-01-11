a0d66d6f5e3c0cc557bbad4e699b5d3eff4f88b2
Danila Argastsev <d.argastsev@adguard.com>
2023-01-11 12:38:06 +0300

    Pull request #485: Feature/AG-18710 Change 2022 to 2023 elsewhere
    Merge in ADGUARD-CORE-LIBS/dns-libs from feature/AG-18710 to master
    
    Squashed commit of the following:
    
    commit 25f571a81ef55aef5cde6b34cfa2120e6474e7fc
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Tue Jan 10 19:41:13 2023 +0300
    
        deleted processorArch
    
    commit 830ad774ed3b6aae69a193f4b62730f99c3dce23
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Tue Jan 10 18:44:02 2023 +0300
    
        Updated packages with new year
    
    commit aa2740fcaf6c33de6ead5b136061902748f03b85
    Merge: 79e921b3 c23f4553
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Tue Jan 10 18:38:35 2023 +0300
    
        Merge branch 'master' into feature/AG-18710
    
    commit 79e921b348e15eadb53167c3b790659b219be1ce
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Tue Jan 10 10:18:00 2023 +0300
    
        updated year
    
    commit 7e833b389b1bf22e44b270a9c362ad2050777eb2
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Tue Jan 10 10:14:47 2023 +0300
    
        updated year

--- platform/windows/cs/Adguard.Dns/Adguard.Dns.TestApp/Properties/AssemblyInfo.cs
+++ platform/windows/cs/Adguard.Dns/Adguard.Dns.TestApp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Adguard.Dns.TestApp")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
