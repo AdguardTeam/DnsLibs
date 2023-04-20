ff6dcc9466d4879dc73ee760fdecbf2948907846
Maxim Grupper <m.grupper@adguard.com>
2023-04-20 18:18:35 +0300

    Pull request #529: Fix docs
    Merge in ADGUARD-CORE-LIBS/dns-libs from AG-21035 to master
    
    Squashed commit of the following:
    
    commit 9cbc4d42b8b42bff231f7431d022c955ce0065f2
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Thu Apr 20 13:47:18 2023 +0300
    
        add refs
    
    commit b651b7184feb4a2da52896ecc56301d527d02690
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Thu Apr 20 11:12:32 2023 +0300
    
        update main pages
    
    commit bafc72825ee5f42a2aa41d6ca3d741c25ba6fbce
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Wed Apr 19 14:13:15 2023 +0300
    
        main page
    
    commit b87caebdc5c533038dc89286cc4befd617f7f8e8
    Author: Max Grupper <m.grupper@adguard.com>
    Date:   Tue Apr 18 13:16:40 2023 +0300
    
        fix refs

--- doxyfiles/capi_main_page.h
+++ doxyfiles/capi_main_page.h
@@ -8,35 +8,49 @@ To use this group of functions, you need to include the dnsproxy.h header file.
 
 @section start Get started
 
-To start working with the proxy server, you need to initialize it using the ag_dnsproxy_init() function.
-Then you can process DNS requests using the ag_dnsproxy_handle_message() function.
-After finishing working with the server, you need to call the ag_dnsproxy_deinit() function.
-Functions are also available to get the current proxy server settings: ag_dnsproxy_get_settings()
-and ag_dnsproxy_settings_get_default().
-To free the memory occupied by settings, use the ag_dnsproxy_settings_free() function.
+To start working with the proxy server, you need to initialize it using the @ref ag_dnsproxy_init() function.
+
+Once initialized, there are two possible ways of handling DNS requests:
+
+1) UDP/TCP Listeners
+Listener configurations are specified in the @ref ag_dnsproxy_settings, and the library user should redirect DNS traffic to this @ref ag_listener_settings::port.
+
+2) Processing UDP packets with @ref ag_dnsproxy_handle_message()
+@note this function cannot handle TCP packets.
+
+If there is a packet tunnel, you can take a straightforward approach and redirect UDP packets from it using this function.
+This function takes packet data and a completion handler block, which will be called after
+the packet is processed and a response packet is returned. If no response packet is required, the method will return nil.
+
+After finishing working with the server, you need to call the @ref ag_dnsproxy_deinit() function.
+
+Functions are also available to get the current proxy server settings: @ref ag_dnsproxy_get_settings()
+and @ref ag_dnsproxy_settings_get_default().
+
+To free the memory occupied by settings, use the @ref ag_dnsproxy_settings_free() function.
 
 @section stamps Working with DNS stamps
 
-To work with DNS stamps, use the ag_dns_stamp_from_str(), ag_dns_stamp_free(), ag_dns_stamp_to_str(), ag_dns_stamp_pretty_url(),
-and ag_dns_stamp_prettier_url() functions.
+To work with DNS stamps, use the @ref ag_dns_stamp_from_str(), @ref ag_dns_stamp_free(), @ref ag_dns_stamp_to_str(), @ref ag_dns_stamp_pretty_url(),
+and @ref ag_dns_stamp_prettier_url() functions.
 
 @section test Useful functions
 
-To check the operability of the upstream server, use the ag_test_upstream() function.
+To check the operability of the upstream server, use the @ref ag_test_upstream() function.
 
-The ag_is_valid_dns_rule() function allows you to check if a string is a valid rule.
+The @ref ag_is_valid_dns_rule() function allows you to check if a string is a valid rule.
 
-The ag_dns_generate_rule_with_options() function is also available for generating DNS request filtering rules.
+The @ref ag_dns_generate_rule_with_options() function is also available for generating DNS request filtering rules.
 
 @section log How to use Logger
 
-To set the logging level, use the ag_set_log_level() function.
+To set the logging level, use the @ref ag_set_log_level() function.
 
-To set the logger callback, use the ag_set_log_callback() function.
+To set the logger callback, use the @ref ag_set_log_callback() function.
 
 @section version How to get version
 
-To get information about the library version, use the ag_dnsproxy_version() function, and to get the C API version, use the ag_get_capi_version() function.
+To get information about the library version, use the @ref ag_dnsproxy_version() function, and to get the C API version, use the @ref ag_get_capi_version() function.
 
 @section example Usage example
 
--- doxyfiles/mac_main_page.h
+++ doxyfiles/mac_main_page.h
@@ -6,47 +6,55 @@
 
 @section start Get started
 
-This Objective-C code represents the public interface of the AGDnsProxy class. To use this API, users should:
+This Objective-C code represents the public interface of the @ref AGDnsProxy class. To use this API, users should:
 
-Create an instance of the AGDnsProxy class using the AGDnsProxy::initWithConfig:handler:error: method.
+- Create an instance of the @ref AGDnsProxy class using the AGDnsProxy::initWithConfig:handler:error: method.
 The method takes the proxy configuration, event handler, and a reference to an <span style="color: red;">NSError *</span> object to be populated with an error if one occurs.
 
-Handle the UDP/TCP packet by sending it to the AGDnsProxy::handlePacket:completionHandler: method.
-The method takes packet data and a completion handler block, which will be called after
-the packet is processed and a response packet is returned. If no response packet is required, the method will return nil.
+- There are two possible ways of handling DNS requests:
 
-Stop the AGDnsProxy by calling the AGDnsProxy::stop method. This method should be called before the AGDnsProxy instance is destroyed.
+    1) UDP/TCP Listeners
+    Listener configurations are specified in the @ref AGDnsProxyConfig, and the library user should redirect DNS traffic to this @ref AGListenerSettings::port.
+
+    2) Processing UDP packets with AGDnsProxy::handlePacket:completionHandler:
+    @note this function cannot handle TCP packets.
+
+    If there is a packet tunnel, you can take a straightforward approach and redirect UDP packets from it using this function.
+    This function takes packet data and a completion handler block, which will be called after
+    the packet is processed and a response packet is returned. If no response packet is required, the method will return nil.
+
+- Stop the @ref AGDnsProxy by calling the @ref AGDnsProxy::stop() method. This method should be called before the @ref AGDnsProxy instance is destroyed.
 
 @section test Useful functions
 
-Check if a string is a valid rule by calling the AGDnsProxy::isValidRule: method. The method takes a string as an argument
+Check if a string is a valid rule by calling the @ref AGDnsProxy::isValidRule: method. The method takes a string as an argument
 and returns true if the string is a rule, and false otherwise.
 
 @section stamps Working with DNS stamps
 
-The AGDnsStamp class provides an API for creating and manipulating DNS stamp objects.
+The @ref AGDnsStamp class provides an API for creating and manipulating DNS stamp objects.
 
-An AGDnsStamp object represents a DNS resolver endpoint that uses either DNSCrypt, DNS-over-HTTPS (DoH), or DNS-over-QUIC (DoQ) protocols.
-The object includes several properties such as protocol type (AGDnsStamp::proto), server IP address (AGDnsStamp::serverAddr), provider name (AGDnsStamp::providerName),
-server public key (AGDnsStamp::serverPublicKey), hashes, path, and several Boolean flags (AGDnsStamp::dnssec, AGDnsStamp::noLog, AGDnsStamp::noFilter) that describe the server's properties.
+An @ref AGDnsStamp object represents a DNS resolver endpoint that uses either DNSCrypt, DNS-over-HTTPS (DoH), or DNS-over-QUIC (DoQ) protocols.
+The object includes several properties such as protocol type (@ref AGDnsStamp::proto), server IP address (@ref AGDnsStamp::serverAddr), provider name (@ref AGDnsStamp::providerName),
+server public key (@ref AGDnsStamp::serverPublicKey), hashes, path, and several Boolean flags (@ref AGDnsStamp::dnssec, @ref AGDnsStamp::noLog, @ref AGDnsStamp::noFilter) that describe the server's properties.
 
 To use this API, users can create an instance of the AGDnsStamp class by calling the AGDnsStamp::initWithString:error:
 or AGDnsStamp::stampWithString:error: method with a string in the <span style="color: red;">"sdns://"</span> format. The string should contain all
-the necessary parameters for initializing an AGDnsStamp object.
+the necessary parameters for initializing an @ref AGDnsStamp object.
 
 Users can also access the object's properties, including the server's IP address and provider name, using the provided getter methods.
 
-Finally, AGDnsStamp provides several convenience methods for creating and representing stamps in different formats,
-including the AGDnsStamp::prettyUrl, AGDnsStamp::prettierUrl, and AGDnsStamp::stringValue properties.
+Finally, @ref AGDnsStamp provides several convenience methods for creating and representing stamps in different formats,
+including the @ref AGDnsStamp::prettyUrl, @ref AGDnsStamp::prettierUrl, and @ref AGDnsStamp::stringValue properties.
 
 @section log How to use Logger
 
 To use this class, you can:
 
-Set the default logging level using the class method AGLogger::setLevel:. This method takes an AGLogLevel value and sets the logging level to be used by default.
+- Set the default logging level using the class method @ref AGLogger::setLevel:. This method takes an AGLogLevel value and sets the logging level to be used by default.
 
-Set the log callback using the class method AGLogger::setCallback:. This method takes a block of type AGLogger::logCallback,
-which is called when a log message needs to be output. The block takes three parameters: the log level of the message (AGLogLevel),
+- Set the log callback using the class method @ref AGLogger::setCallback:. This method takes a block of type @ref AGLogger::logCallback,
+which is called when a log message needs to be output. The block takes three parameters: the log level of the message (@ref AGLogLevel),
 the formatted log message <span style="color: red;">(const char *)</span>, and the length of the log message (int).
 
 When using this class, you should set the logging level and callback function as needed for application.
@@ -55,7 +63,7 @@ the message as appropriate (e.g., print to console, write to log file, etc.).
 
 @section version How to get version
 
-Get the DNS proxy library version by calling the static AGDnsProxy::libraryVersion method.
+Get the DNS proxy library version by calling the static @ref AGDnsProxy::libraryVersion method.
 The method returns a string with the version of the library.
 
 @section example Usage example
