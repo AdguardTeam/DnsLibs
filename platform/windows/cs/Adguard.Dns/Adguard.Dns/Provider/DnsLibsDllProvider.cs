07e57b564b1e2f59f3d2093561dad63a19dec50f
Danila Argastsev <d.argastsev@adguard.com>
2023-11-30 14:38:58 +0300

    Pull request #601: feature/AG-27853 Extract ILibsDllProvider to the base utils
    Merge in ADGUARD-CORE-LIBS/dns-libs from feature/AG-27853 to master
    
    Squashed commit of the following:
    
    commit 3749280f2c561be8b9dde5f254c3ba30a837126e
    Author: Danila-Arg <d.argastsev@adguard.com>
    Date:   Thu Nov 30 12:33:20 2023 +0300
    
        using ILibsDllProvider from Base utils

--- platform/windows/cs/Adguard.Dns/Adguard.Dns/Provider/DnsLibsDllProvider.cs
+++ platform/windows/cs/Adguard.Dns/Adguard.Dns/Provider/DnsLibsDllProvider.cs
@@ -1,17 +1,15 @@
 ﻿using AdGuard.Utils.Base.DriverInstaller;
-using AdGuard.Utils.Base.Interop;
-using AdGuard.Utils.Base.Logging;
-using AdGuard.Utils.Base.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using AdGuard.Utils.Base.DllProvider;
 
 namespace Adguard.Dns.Provider
 {
 	/// <summary>
 	/// Dnslibs dll provider 
 	/// </summary>
-	public class DnsLibsDllProvider : IDnsLibsDllProvider
+	public class DnsLibsDllProvider : LibsDllProviderBase
 	{
 		private const string WIN32_DNS_LIBS_DLL_NAME = @"x86\AdguardDns.dll";
 		private const string WIN64_DNS_LIBS_DLL_NAME = @"x64\AdguardDns.dll";
@@ -66,44 +64,10 @@ namespace Adguard.Dns.Provider
 			};
 
 		/// <summary>
-		/// Gets the Dll functions instance
-		/// </summary>
-		// ReSharper disable once InconsistentNaming
-		private readonly DynamicDllFunctions m_DnsDynamicDllFunctions;
-
-		/// <summary>
 		/// Dns libs dll provider
 		/// </summary>
-		public DnsLibsDllProvider()
+		public DnsLibsDllProvider() : base(DNS_LIBS_DLL_PATHES_MAP)
 		{
-			DnsLibsDllPath = GetDnsLibsDllPath();
-			DynamicDllFunctions dnsLibsDllFunctions = new DynamicDllFunctions(DnsLibsDllPath);
-			m_DnsDynamicDllFunctions = dnsLibsDllFunctions;
-		}
-
-		/// <summary>
-		/// Invokes function (with non-void return) with the specified <see cref="functionName"/>
-		/// and passed parameters, and return the result with the specified type
-		/// </summary>
-		/// <param name="functionName">Function name</param>
-		/// <param name="args">Collection of parameters</param>
-		/// <typeparam name="TReturn">Return type</typeparam>
-		/// <returns>Function result</returns>
-		public TReturn InvokeFunction<TReturn>(string functionName, params object[] args)
-		{
-			TReturn result = m_DnsDynamicDllFunctions.InvokeFunction<TReturn>(functionName, args);
-			return result;
-		}
-
-		/// <summary>
-		/// Invokes function (with void return) with the specified <see cref="functionName"/>
-		/// and passed parameters
-		/// </summary>
-		/// <param name="functionName">Function name</param>
-		/// <param name="args">Collection of parameters</param>
-		public void InvokeFunction(string functionName, params object[] args)
-		{
-			m_DnsDynamicDllFunctions.InvokeFunction(functionName, args);
 		}
 
 		private static readonly Lazy<DnsLibsDllProvider> LAZY =
@@ -114,7 +78,7 @@ namespace Adguard.Dns.Provider
 		/// <summary>
 		/// Gets a singleton instance of <see cref="DnsLibsDllProvider"/> object
 		/// </summary>
-		public static IDnsLibsDllProvider Instance
+		public static ILibsDllProvider Instance
 		{
 			get { return LAZY.Value; }
 		}
@@ -125,57 +89,5 @@ namespace Adguard.Dns.Provider
 		/// Gets or sets dnslibs dll path
 		/// </summary>
 		public string DnsLibsDllPath { get; private set; }
-
-		/// <summary>
-		/// Gets the cert utils dll name, according to the operation system's architecture
-		/// <exception cref="InvalidOperationException">Thrown, if
-		/// current OS architecture is not recognized</exception>
-		/// </summary>
-		private string GetDnsLibsDllPath()
-		{
-			ArchitectureLocal architecture = WindowsTools.GetOsArchitecture();
-			if (!DNS_LIBS_DLL_PATHES_MAP.TryGetValue(
-					architecture, out KeyValuePair<string, string> dnsLibsDllPathPair))
-			{
-				throw new InvalidOperationException(
-					$"Can't get DnsLibs dll name path because of wrong architecture type - {architecture}");
-			}
-
-			string dllName =
-				GetDnsLibsDllPath(dnsLibsDllPathPair.Key, dnsLibsDllPathPair.Value);
-			return dllName;
-		}
-
-		/// <summary>
-		/// Gets the cert utils dll name, according to the passed prior and aux dll names
-		/// <exception cref="InvalidOperationException">Thrown, if
-		/// current OS architecture is not recognized</exception>
-		/// </summary>
-		private static string GetDnsLibsDllPath(string priorDllName, string auxDllName)
-		{
-			string dnsLibsDllName;
-			try
-			{
-				using (new DynamicDllFunctions(priorDllName))
-				{
-					dnsLibsDllName = priorDllName;
-				}
-			}
-			catch (InvalidOperationException ex)
-			{
-				Logger.QuietWarn(ex, "Simple detection of OS bitness failed: {0}({1}), IntPtr size is {2}",
-					Environment.Is64BitOperatingSystem,
-					Environment.Is64BitProcess,
-					IntPtr.Size);
-				dnsLibsDllName = auxDllName;
-			}
-
-			return dnsLibsDllName;
-		}
-
-		public void Dispose()
-		{
-			m_DnsDynamicDllFunctions?.Dispose();
-		}
 	}
 }
