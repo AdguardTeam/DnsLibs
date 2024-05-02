76c022c22b6a6481864d1aaa95f170fdf9dab871
Sergey Fionov <sfionov@adguard.com>
2024-05-02 22:41:50 +0300

    skipci: Fix Apple toolchain

--- conan/profiles/apple.toolchain.cmake
+++ conan/profiles/apple.toolchain.cmake
@@ -1,13 +1,3 @@
-# Set minimum deployment version
-if(CMAKE_OSX_SYSROOT STREQUAL iphonesimulator)
-    set(CMAKE_OSX_DEPLOYMENT_TARGET "13.0" CACHE STRING "Minimum iOS deployment version")
-elseif(CMAKE_OSX_SYSROOT STREQUAL iphoneos)
-    set(CMAKE_OSX_DEPLOYMENT_TARGET "13.0" CACHE STRING "Minimum iOS deployment version")
-    add_compile_options(-fembed-bitcode)
-else()
-    set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15" CACHE STRING "Minimum macOS deployment version")
-endif()
-
 # We don't need to export any symbol implicitly, and also disable C++ exceptions.
 set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fvisibility=hidden -fno-exceptions")
 set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -fvisibility=hidden -fno-exceptions")
