6ec10f90c2e36264557bbc84ccd5a6812f464c20
Nikita Gorskikh <n.gorskikh@adguard.com>
2024-09-03 15:56:16 +0300

    Pull request #671: Disable frame pointer omission when compiling Conan dependencies
    Merge in ADGUARD-CORE-LIBS/dns-libs from feature/update_toolchain_files to master
    
    Squashed commit of the following:
    
    commit 7e26c26fc3c88554f1f1ea8abd8c3fb6ffd293c7
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Tue Sep 3 15:02:36 2024 +0300
    
        Disable frame pointer omission when compiling Conan dependencies

--- conan/profiles/apple.toolchain.cmake
+++ conan/profiles/apple.toolchain.cmake
@@ -1,3 +1,3 @@
 # We don't need to export any symbol implicitly, and also disable C++ exceptions.
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fvisibility=hidden -fno-exceptions")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -fvisibility=hidden -fno-exceptions")
+set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fvisibility=hidden -fno-exceptions -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -fvisibility=hidden -fno-exceptions -fno-omit-frame-pointer")
