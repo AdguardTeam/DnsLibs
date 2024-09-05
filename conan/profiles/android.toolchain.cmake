f17a8f61d15c8ba84cb25dc7333b122a068f737b
Nikita Gorskikh <n.gorskikh@adguard.com>
2024-09-05 15:45:48 +0300

    Pull request #672: Use the published Conan files from NativeLibsCommon
    Merge in ADGUARD-CORE-LIBS/dns-libs from feature/use_published_conan_files to master
    
    Squashed commit of the following:
    
    commit cea187aedc1c793905d74d659f02daa042932c8a
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Wed Sep 4 23:42:25 2024 +0300
    
        Use the published Conan files from NativeLibsCommon

--- conan/profiles/android.toolchain.cmake
+++
@@ -1,3 +0,0 @@
-# We don't need to export any symbol implicitly, and also disable C++ exceptions, and enable unwind tables.
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -fvisibility=hidden -fno-exceptions -funwind-tables -fno-omit-frame-pointer")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -fvisibility=hidden -fno-exceptions -funwind-tables -fno-omit-frame-pointer")
--- conan/profiles/linux.toolchain.cmake
+++
@@ -1,5 +0,0 @@
-# We don't need to export any symbol implicitly, and also disable C++ exceptions, and enable unwind tables.
-# add -D__ASM_SIGCONTEXT_H because it is already part of signal.h
-set(CMAKE_SYSTEM_NAME Linux)
-set(CONAN_C_FLAGS "-fvisibility=hidden -fno-exceptions -funwind-tables -D__ASM_SIGCONTEXT_H -fno-omit-frame-pointer")
-set(CONAN_CXX_FLAGS "-fvisibility=hidden -fno-exceptions -funwind-tables -D__ASM_SIGCONTEXT_H -fno-omit-frame-pointer")
