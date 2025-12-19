607b991b508b27de4c436453c086e0dffbe77de7
Bamboo <Bamboo>
2025-12-19 07:32:49 +0000

    skipci: Automatic version increment by Bamboo

--- platform/windows/cs/Adguard.Dns/SolutionInfo.cs
+++ platform/windows/cs/Adguard.Dns/SolutionInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Adguard Software Ltd 2009-2024")]
 [assembly: AssemblyTrademark("Copyright © Adguard Software Ltd 2024")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.8.3")]
-[assembly: AssemblyFileVersion("2.8.3")]
+[assembly: AssemblyVersion("2.8.4")]
+[assembly: AssemblyFileVersion("2.8.4")]
